ca11a851823959d7c0ce1f4e275be878adfd6ad6
Eduardo Serafim <eduardo@example.com>
2025-06-25 08:18:33 -0300

    atualização rota

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, abort
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import check_password_hash
+from datetime import datetime
 from app import db
 from models import Usuario, PerfilTrabalhador, PerfilCliente, PerfilLojista, PerfilAdministrador, Servico, Material, SolicitacaoServico
 from forms import (FormularioLogin, FormularioRegistro, FormularioPerfilTrabalhador, 
